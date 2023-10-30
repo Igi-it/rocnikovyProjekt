9bcff1c779983ed2efe5c7f945d4497c5f39a551
Igi-it <igi701580@gmail.com>
2023-10-30 06:16:29 +0100

    pridano dsp

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,8 +14,7 @@
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor,
-									  public juce::Slider::Listener
+class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -24,13 +23,11 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
-	void sliderValueChanged (juce::Slider* slider);
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
-
-	juce::Slider gainSlider;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
